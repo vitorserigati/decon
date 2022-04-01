78739381167e9f6b6401d7590acff556bf7bc54a
Vitor Serigati <vitor_serigati@hotmail.com>
2022-04-01 06:00:54 -0300

    Fase inicial do programa completa

---
+++ README.md
@@ -0,0 +1,33 @@
+# Decon
+---
+        Projeto interdisciplinar que visa unir as matérias de Organização e Arquitetura de computadores e também Programação de computadores. 
+        O projeto consiste em criar um programa que possa fazer as conversões da base decimal para as bases binária, hexadecimal e octal.
+    
+    Proposto pelo curso de Análise e Desenvolvimento de Sistemas da Universidade Cruzeiro do Sul
+
+---
+
+## Conteúdo:
+        O programa conta com um menú, onde pode-se escolher a base para a qual o usuário deseja converter o número que ele mesmo irá digitar.
+
+        O programa deverá, então, fazer os cálculos necessários para que seja apresentado ao usuário.
+
+---
+
+## Linguagem utilizada:
+
+        Python -versão- 3.10
+
+---
+
+## Criadores:
+
+        Vitor Serigati de Oliveira Basso
+        Henrique Serigati de Oliveira Basso
+        Alisson Cavalcante da Silva
+        Danilo Paz
+    
+---
+
+## Versão do Sistema:
+        1.0.1
---
+++ decon.py
@@ -0,0 +1,52 @@
+def calculator(opcao, valor): #Criação da Função para o cálculo, com a opção escolhida e valor informados
+    resultado = ''  #Resultado final das contas
+    if (opcao == 1):    #verifica a opção. Se for igual a 1, realizará a covnersão para binário
+        sequencia = '' #sequencia recebida das contas
+        valorcont = valor #atribuição do valor recebido à variável usada no looping
+        resto = 0  
+        while (valorcont > 0): #enquanto o valor da conta for maior que 0 a operação se repete
+            resto = valorcont%2 #atribui o resto da conta à variável resto 
+            sequencia += str(resto) #transforma a variável resto em string, enquanto concatena com o valor anterior
+            valorcont = valorcont//2 #Reatribui o valor de contagem com o resultado da divisão pela base (arredondado)
+            resultado = sequencia[::-1] #atribui a sequencia de forma invertida ao resultado final
+    elif (opcao == 2):  #verifica a opção. Se for igual a 1, realizará a covnersão para hexadecimal
+        sequencia = ''
+        valorcont = valor
+        resto = 0
+        while (valorcont > 0):
+            
+            if (valorcont%16 == 10): #verifica se o valor é igual a 10
+                resto = 'A' #caso o valor seja igual a 10, atribui a letra A ao resto
+            elif (valorcont%16 == 11):
+                resto = 'B'
+            elif (valorcont%16 == 12):
+                resto = 'C'
+            elif (valorcont%16 == 13):
+                resto = 'D'
+            elif (valorcont%16 == 14):
+                resto = 'E'
+            elif (valorcont%16 == 15):
+                resto = 'F'
+            else:
+                resto = valorcont%16
+            sequencia += str(resto)
+            valorcont = valorcont//16
+            resultado = sequencia[::-1]
+    elif(opcao == 3):   #verifica a opção. Se for igual a 1, realizará a covnersão para octal
+        sequencia = ''
+        valorcont = valor
+        resto = 0
+        while(valorcont > 0):
+            resto = valorcont%8
+            sequencia += str(resto)
+            valorcont = valorcont//8
+            resultado = sequencia [::-1]
+    return f'O resultado da conversão é: {resultado}'   #retorna o resultado da função.
+
+#Início da interação com o Usuário
+
+print('Escolha uma opção: \n[1] = Conversor de Decimal para binário: \n[2] = Conversor de decimal para Hexadecimal: \n[3] = Conversor de decimal para octal: ')
+opcao = int(input())#atribuição da opção escolhida à variável
+valor = int(input('Digite o valor que deseja converter: ')) #valor escolhido é digitado e guardado
+print(calculator(opcao,valor)) #imprime o resultado da função com os argumentos escolhidos
+
